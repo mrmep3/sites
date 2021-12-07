097d9bb8527d76034d9b142f8340ad5452e7f591
Dmitry <46598644+mrmep3@users.noreply.github.com>
2021-12-07 12:18:25 +0300

    chande address css

--- liontech_events.php
+++ liontech_events.php
@@ -1,6 +1,6 @@
 <?php 
 $content = file_get_contents('https://calendar.google.com/calendar/u/0/embed?src=c_6j3h69e15acmqr0u58h8cheiso@group.calendar.google.com&ctz=Europe/Moscow&hl=ru&mode=AGENDA');
 $content = str_replace('</title>','</title><base href="https://calendar.google.com/calendar/u/0/r" />', $content);
-$content = str_replace('</head>', '<link rel="stylesheet" href="https://github.com/mrmep3/liontech/blob/master/liontech_styles.css" /></head>', $content);
+$content = str_replace('</head>', '<link rel="stylesheet" href="https://mrmep3.github.io/sites/liontech_styles.css" /></head>', $content);
 $content = str_replace('<body style="background-color:#fff">', '<body style="background-color:#fee500">', $content);
 echo $content;
